51d5b13a9fc13497b490acda2cd9965f799fab88
Sagar Parker <sagar8parker@gmail.com>
2020-10-25 12:12:16 +0530

    Basic form with react hooks

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState,useEffect } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [name,setName] = useState("Sagar");
+  const [age,setAge] = useState('30');
+  const [output,setOutput] = useState(false);
+
+  const clickHandler = () =>{
+    setOutput(true);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter your name</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Enter your name"
+        onChangeText={(val)=> setName(val)}/>
+      <Text>Enter your age</Text>
+      <TextInput 
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Enter your age"
+        onChangeText={(val)=> setAge(val)}/>
+      <View style={styles.buttonContainer}>
+        <Button title="Update name"  onPress={clickHandler} />
+      </View>
+      {output && <Text>
+            Hello {name} {age}
+      </Text>}
     </View>
   );
 }
@@ -16,6 +39,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  buttonContainer:{
+    marginTop:10
+  },
+  input:{
+    borderWidth:1,
+    borderColor:'gainsboro',
+    width:150,
+    marginTop:10,
+    marginBottom:10,
+    padding:10 
+  }
 });
